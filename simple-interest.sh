a4a4fb31db44c940b94fc0d77601378687e369b3
shadab0908 <164162731+shadab0908@users.noreply.github.com>
2025-11-04 02:35:33 +0530

    Update author in simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # shadab0908
    # Input:
    # p, principal amount
    # t, time period in years
